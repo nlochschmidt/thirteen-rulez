6dcef090d64934bd3adccf9e7fc2f782286080c8
Niklas Lochschmidt <nlochschmidt@gmail.com>
2016-03-23 22:25:40 +0100

    Finally!
    I wonder if I should add a license and publish to npm...

---
+++ README.md
@@ -0,0 +1,14 @@
+# Thirteen rulez
+
+I always wondered whether "1" and "337" multiplied by thirteen are still thirteen.
+
+Try it yourself:
+
+```bash
+npm install
+node index.js
+```
+
+Many thanks to [@jezenthomas](https://twitter.com/jezenthomas) and 
+[@epic_science](https://twitter.com/epic_science). I couldn't have made it 
+without you.
---
+++ index.js
@@ -0,0 +1,13 @@
+var thirteen = require('thirteen');
+var isThirteen = require('is-thirteen');
+
+var someNumbers = ["1", "337"];
+
+for (index in someNumbers) {
+	var aNumber = someNumbers[index]
+	if (isThirteen(thirteen(aNumber))) {
+		console.log('YES!!! ' + aNumber + ' times 13 is thirteen 💖');
+	} else {
+		console.log('I\'m sorry! ' + aNumber + ' times 13 is not thirteen 😢');
+	}
+}
